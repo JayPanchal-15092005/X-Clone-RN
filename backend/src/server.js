1fac308f6b8faf0b33c20ae6a39a3e43bca8086e
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-08-27 18:41:35 +0530

    X Clone React native Commit

---
+++ backend/src/server.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
+// app.set("trust proxy", true);
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
+
+// app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:", ENV.PORT));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    // listen for the local development not for the production development 
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () =>
+        console.log("Server is up and running on PORT:", ENV.PORT)
+      );
+    }
+  } catch (error) {
+    console.log("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+// export for the vercel
+export default app;
